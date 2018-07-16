2b97fe089d8a30470b2c6458dc7e16ffcc152c1b
suzuki.sh <s2s@live.jp>
2018-07-16 17:36:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# read you (i_read_u)
+Extracting commands from README markdown file.
+
+## Installation
+git clone and run this.
+
+```
+$ npm run build
+$ sudo npm link
+```
+
+## Usage
+
+```
+$ ireadu
+```
+
+### Test Commands (Try ireadu command and hit this)
+
+```
+$ ls
+>w
+ # pwd
+id
+```
+
+## License
+MIT.
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as child_process from "child_process";
+import * as inquirer from "inquirer";
+import * as marked from "marked";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+
+let filePath = "README.md";
+
+fs.readFile(filePath, "utf8", (err, file) => {
+  if (err) {
+    console.error(err);
+  }
+  const html = marked(file);
+
+  let $ = cheerio.load(html);
+
+  let commands = {};
+  let key;
+  $('*').toArray().map((e)=> {
+    let $e = cheerio(e);
+    if ($e.is('h1,h2,h3,h4,h5,h6')) {
+      key = $e.text();
+    }
+    if (key && $e.is('code')) {
+      $e.text().split(/\r\n|\r|\n/).map((command)=> {
+        if (!commands[key]) { commands[key] = [] }
+        commands[key].push(command);
+      });
+    }
+  });
+
+  let question = {
+    "type": "list",
+    "name": "section",
+    "message": "choice md",
+    "choices": Object.keys(commands),
+  }
+
+  inquirer.prompt([question]).then((answer) => {
+    let question = {
+      "type": "list",
+      "name": "command",
+      "message": "choice md",
+      "choices": commands[answer["section"]],
+    }
+    inquirer.prompt([question]).then((answer) => {
+      child_process.exec(answer["command"].replace(/^[#>\s\$]+/,''), function (err, stdout, stderr) {
+        console.log(stdout);
+      });
+    })
+  });
+});
