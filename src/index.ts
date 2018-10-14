a963d66a831143c05353984017ab7f025d7792ec
suzuki.sh <s2s@live.jp>
2018-10-15 00:49:56 +0900

    add type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,21 @@
-import * as cheerio from "cheerio";
 import * as child_process from "child_process";
-import * as fs from "fs";
 import * as inquirer from "inquirer";
-import * as marked from "marked";
-const program = require("commander");
+const commander = require("commander");
+import { CommandSections } from "./commandSections";
+import { StringCompiledHTML } from "./stringCompiledHTML";
 
-function markdownToHtml(filepath: string): string {
-  let html: string;
-
-  const file = fs.readFileSync(filepath, "utf8");
-  html = marked(file);
-
-  return html;
-}
-
-function htmlToCommands(html: string): { [key: string]: string[] } {
-  const $ = cheerio.load(html);
-
-  const commands = {};
-  let key;
-  $("*")
-    .toArray()
-    .map(e => {
-      const $e = cheerio(e);
-      if ($e.is("h1,h2,h3,h4,h5,h6")) {
-        const heading: number = +$e.get(0).tagName.substr(1, 1);
-        key = `${"#".repeat(heading)} ${$e.text()}`;
-      }
-      if (key && $e.is("code")) {
-        $e.text()
-          .split(/\r\n|\r|\n/)
-          .map(command => {
-            if (!commands[key]) {
-              commands[key] = [];
-            }
-            commands[key].push(command.replace(/^[#>\s\$]*/, " "));
-          });
-      }
-    });
-
-  return commands;
-}
-
-function executeCommands(commands: { [key: string]: string[] }) {
+function executeCommands(commands: CommandSections): void {
   const command = "command";
   const commandChoices = [];
 
-  Object.keys(commands).forEach(sect => {
+  Object.keys(commands.sections).forEach(sect => {
     commandChoices.push(new inquirer.Separator(sect));
-    commands[sect].forEach(cmd => {
+    commands.sections[sect].forEach(cmd => {
       commandChoices.push(cmd);
     });
   });
 
-  const questionCommand = {
+  const questionCommand: inquirer.Question = {
     type: "list",
     name: command,
     message: "choice command",
@@ -66,15 +28,15 @@ function executeCommands(commands: { [key: string]: string[] }) {
   });
 }
 
-function main() {
-  const packagejson = require("../package.json");
+function main(): void {
+  const packagejson: any = require("../package.json");
 
-  program.option("--file <filename>", "Specify the file name", "README.md");
-  program.version(packagejson.version);
+  commander.option("--file <filename>", "Specify the file name", "README.md");
+  commander.version(packagejson.version);
 
-  program.parse(process.argv);
-  const html = markdownToHtml(program.file);
-  const commands = htmlToCommands(html);
+  commander.parse(process.argv);
+  const html = StringCompiledHTML.generateFromMarkdownFile(commander.file);
+  const commands = CommandSections.generateFromHTML(html);
   executeCommands(commands);
 }
 
