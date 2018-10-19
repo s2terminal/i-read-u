da16e8125ca6239ddd074551e49eaffdc89fad23
suzuki.sh <s2s@live.jp>
2018-10-19 21:02:52 +0900

    add commandSections test

--- src/stringCompiledHTML.test.ts
+++ src/stringCompiledHTML.test.ts
@@ -1,9 +1,10 @@
+import { CommandSections } from "./commandSections";
 import { StringCompiledHTML } from "./stringCompiledHTML";
 
-describe('StringCompiledHTML', () => {
+describe("StringCompiledHTML", () => {
   const html = StringCompiledHTML.generateFromMarkdownFile("./test/DUMMY.md");
 
-  it('generate HTML', () => {
+  it("generate HTML", () => {
     expect(html.string).toEqual(`<h1 id="first-commands">First Commands</h1>
 <pre><code>$ ls
 &gt;w</code></pre><h2 id="second-commands">Second Commands</h2>
@@ -12,4 +13,14 @@ describe('StringCompiledHTML', () => {
   });
 });
 
+describe("CommandSections", () => {
+  const html = StringCompiledHTML.generateFromMarkdownFile("./test/DUMMY.md");
+  const commands = CommandSections.generateFromHTML(html);
 
+  it("generate commands", () => {
+    expect(commands.sections).toEqual({
+      "# First Commands": [" ls", " w"],
+      "## Second Commands": [" pwd", " id"]
+    });
+  });
+});
