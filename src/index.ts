2bac7e45bb26805dbf398673ae2ef2ab67398b00
suzuki.sh <s2s@live.jp>
2018-12-02 11:17:23 +0900

    catch error form file reading

--- src/index.ts
+++ src/index.ts
@@ -15,31 +15,22 @@ interface IArgv {
 function main(): void {
   const __ = i18n();
 
-  try {
-    const args = configureCommander();
-    const content = read(args.file);
-    const html = StringCompiledHTML.generateFromMarkdownContent(content);
-    const commands = html.toCommandSections();
+  const args = configureCommander();
+  const content = read(args.file, __);
+  const html = StringCompiledHTML.generateFromMarkdownContent(content);
+  const commands = html.toCommandSections();
 
-    commands.choiceOne(__("question"), (questionCommand, questionName) => {
-      inquirer.prompt([questionCommand]).then(answerCommands => {
-        const cmd = child_process.exec(answerCommands[questionName]);
-        cmd.stdout.pipe(process.stdout);
-        cmd.stderr.pipe(process.stderr);
-      });
-    });
-  } catch (e) {
-    if (e instanceof Error) {
-      if (e.code === "ENOENT") {
-        // tslint:disable-next-line:no-console
-        console.log(__("FileNotFound"));
-      } else {
-        throw e;
-      }
-    } else {
-      throw e;
-    }
+  if (commands.sections.length === 0) {
+    return;
   }
+
+  commands.choiceOne(__("question"), (questionCommand, questionName) => {
+    inquirer.prompt([questionCommand]).then(answerCommands => {
+      const cmd = child_process.exec(answerCommands[questionName]);
+      cmd.stdout.pipe(process.stdout);
+      cmd.stderr.pipe(process.stderr);
+    });
+  });
 }
 
 function configureCommander(): IArgv {
@@ -52,8 +43,23 @@ function configureCommander(): IArgv {
   return { file: commander.file };
 }
 
-function read(filepath: string): string {
-  return fs.readFileSync(format(parse(filepath)), "utf8");
+function read(filepath: string, __: (key: string) => string): string {
+  try {
+    return fs.readFileSync(format(parse(filepath)), "utf8");
+  } catch (e) {
+    if (e instanceof Error) {
+      if (e.message.indexOf("ENOENT") === 0) {
+        // tslint:disable-next-line:no-console
+        console.log(__("FileNotFound"));
+      } else {
+        throw e;
+      }
+    } else {
+      throw e;
+    }
+  }
+
+  return "";
 }
 
 function i18n() {
